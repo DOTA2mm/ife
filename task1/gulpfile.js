bea32df1c4980aef27daffb586b03ce024cf0659
fedt <dota2mm@163.com>
2017-03-02 23:48:17 +0800

    modi gulp config

--- task1/gulpfile.js
+++ task1/gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp')
 var browsersync = require('browser-sync').create()
+var reload = browsersync.reload
 var less = require('gulp-less')
 
 // less处理
 gulp.task('style', function () {
-  gulp.src('style/*.{less, css}')
+  gulp.src('style/*.less')
     .pipe(less()) // 编译less
     .pipe(gulp.dest('dist/style')) // 生成css
-    .pipe(browsersync.stream())
+    .pipe(reload({stream: true}))
 })
 
 // 自动刷新
@@ -16,10 +17,10 @@ gulp.task('server', function () {
   browsersync.init({
     port: 8086,
     server: {
-      baseDir: ['dist', 'index.html']
+      baseDir: './'
     }
   })
-  gulp.watch('style/*.{less, css}', ['style'])
-  gulp.watch('*.html')
+  gulp.watch('style/*.less', ['style'])
+  gulp.watch('index.html').on('change', reload)
 })
 gulp.task('default', ['server'])
