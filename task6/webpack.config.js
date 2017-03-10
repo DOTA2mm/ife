d3de47a7dff98268a2f2736f5e46f7faa4cfe0b9
Chuck <dota2mm@163.com>
2017-03-10 18:33:43 +0800

    engineering configuration

---
+++ task6/webpack.config.js
@@ -0,0 +1,57 @@
+module.exports = function (env = {}) {
+  const webpack = require('webpack')
+  const path = require('path')
+  const fs = require('fs')
+  const packageConf = JSON.parse(fs.readFileSync('package.json'), 'utf-8')
+
+  let name = packageConf.name
+  let version = packageConf.version
+  let library = name.replace(/^(\w)/, m => m.toUpperCase())
+  let proxPort = 8081
+  let plugins = []
+  let loaders = []
+
+  if (env.production) {
+    name += `-${version}.min`
+    // compress js in production environment
+    plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: false
+        }
+      })
+    )
+  }
+
+  if (fs.existsSync('./.babelrc')) {
+    // use babel
+    let babelConf = JSON.parse(fs.readFileSync('.babelrc'))
+    loaders.push({
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: 'babel-loder',
+      query: babelConf
+    })
+  }
+
+  return {
+    entry: './lib/app.js',
+    output: {
+      filename: `${name}.js`,
+      path: path.resole(__dirname, 'dist'),
+      publicPath: '/static/js/',
+      library: `${library}`,
+      libraryTarget: 'umd'
+    },
+    plugins: plugins,
+    module: {
+      loaders: loaders
+    },
+    devServer: {
+      proxy: {
+        '*': `http://127.0.0.1:${proxPort}`
+      }
+    }
+  }
+}
